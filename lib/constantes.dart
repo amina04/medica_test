7d3f4b5d64247f2ca8ac455f324b0d9a0d7f8266
amina04 <khachaamina0@gmail.com>
2020-04-28 15:08:33 +0200

    crée fenetre de fin journéé

--- lib/constantes.dart
+++ lib/constantes.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const klabelTextStyle = TextStyle(
     fontSize: 16.4, color: Colors.black54, fontWeight: FontWeight.w400);
 const klabelresultatStyle =
-    TextStyle(fontSize: 17, color: Colors.black, fontWeight: FontWeight.w500);
+    TextStyle(fontSize: 15.2, color: Colors.black, fontWeight: FontWeight.w500);
 const kappBarTextStyle = TextStyle(fontSize: 22, color: Colors.white);
 const kbuttonTextStyle =
     TextStyle(fontSize: 16.4, color: Colors.white, fontWeight: FontWeight.w700);
--- lib/view/fin_journe_screen.dart
+++ lib/view/fin_journe_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gradient_app_bar/gradient_app_bar.dart';
+import 'package:medica/constantes.dart';
 
 class Fin_journee extends StatefulWidget {
   static String id = 'fin_journee_screen';
@@ -10,7 +13,155 @@ class _Fin_journeeState extends State<Fin_journee> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow,
+      appBar: GradientAppBar(
+        title: Text('Fin de journée', style: kappBarTextStyle),
+        gradient:
+            LinearGradient(colors: [Colors.lightBlueAccent, Colors.tealAccent]),
+      ),
+      //Navigateur Bar
+      body: Column(
+        children: <Widget>[
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'choisis un médicament',
+                    style: klabelresultatStyle,
+                  ),
+                  SizedBox(
+                    width: 30.0,
+                  ),
+                  // TODO: create a dropdown menu from a data base
+                  DropdownButton<String>(items: null, onChanged: null),
+                ],
+              ),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(vertical: 80.0),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(20.0, 48.0, 0, 48.0),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          child: Text(
+                            'La quantité consommée : ',
+                            style: klabelTextStyle,
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            children: <Widget>[
+                              Text(
+                                '5',
+                                style: klabelresultatStyle,
+                              ),
+                              Text(
+                                'flacons',
+                                style: klabelresultatStyle,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          child: Text(
+                            'reliquats et les stabilités : ',
+                            style: klabelTextStyle,
+                          ),
+                        ),
+                        Container(
+                          child: Column(
+                            //relicat
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    'Relicats :',
+                                    style: klabelresultatStyle,
+                                  ),
+                                  Text(
+                                    '5',
+                                    style: klabelresultatStyle,
+                                  ),
+                                  Text(
+                                    'flacons',
+                                    style: klabelresultatStyle,
+                                  ),
+                                ],
+                              ),
+                              //stabilite
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    'Stabilités :',
+                                    style: klabelresultatStyle,
+                                  ),
+                                  Text(
+                                    '48',
+                                    style: klabelresultatStyle,
+                                  ),
+                                  Text(
+                                    'heures ',
+                                    style: klabelresultatStyle,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          child: Text(
+                            'le prix des reliquats jetés : ',
+                            style: klabelTextStyle,
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            children: <Widget>[
+                              Text(
+                                '5623.15',
+                                style: klabelresultatStyle,
+                              ),
+                              Text(
+                                'DA',
+                                style: klabelresultatStyle,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

eed26f0ddcfb4a04071b033cffe5158cc1820448
amina04 <khachaamina0@gmail.com>
2020-05-07 19:16:22 +0200

    créé la liste des médicaments

--- lib/view/add_med_screen.dart
+++ lib/view/add_med_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medica/widgets_sp%C3%A9cifiques/TextFieldmedica.dart';
 
 class Add_med extends StatefulWidget {
   static String id = 'Add_med';
@@ -15,7 +16,10 @@ class _Add_med extends State<Add_med> {
         child: Container(
           padding: EdgeInsets.all(30.0),
           child: Column(
-            children: <Widget>[],
+            children: <Widget>[
+              Textfieldmedica(label: 'Nom de médicament'),
+
+            ],
           ),
         ),
       ),
